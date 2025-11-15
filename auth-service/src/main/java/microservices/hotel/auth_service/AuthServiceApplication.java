579a0bb5219ade0210633400fd0f2d350ae5caee
J4F3ET <jafetdc16@gmail.com>
2025-11-15 14:43:30 -0500

    feat: Update configuration for microservices and enhance logging setup

--- auth-service/src/main/java/microservices/hotel/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/microservices/hotel/auth_service/AuthServiceApplication.java
@@ -1,14 +1,17 @@
 package microservices.hotel.auth_service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@Slf4j
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
+        log.info("TEST DE CONEXION ELK");
 		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
