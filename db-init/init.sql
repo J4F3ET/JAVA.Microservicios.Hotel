248485eb96c8ba013f4c5b9ebd98279e534bc914
J4F3ET <jafetdc16@gmail.com>
2025-11-18 07:35:12 -0500

    chore: Update Dockerfiles and SQL initialization scripts for consistency

--- db-init/init.sql
+++ db-init/init.sql
@@ -1,18 +1,12 @@
-CREATE DATABASE db_users;
-CREATE DATABASE db_payments;
-CREATE DATABASE db_hotel;
+CREATE USER uuser WITH PASSWORD 'admin-password-uuser*';
+CREATE USER uhotel WITH PASSWORD 'admin-password-uhotel*';
+CREATE USER upayment WITH PASSWORD 'admin-password-upayment*';
+
+CREATE DATABASE db_users OWNER uuser;
+CREATE DATABASE db_payments OWNER upayment;
+CREATE DATABASE db_hotel OWNER uhotel;
 
 CREATE USER uauth WITH PASSWORD 'admin-password-uauth*';
 GRANT CONNECT ON DATABASE db_users TO uauth;
 GRANT USAGE ON SCHEMA public TO uauth;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO uauth;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO uauth;
-
-CREATE USER uuser WITH PASSWORD 'admin-password-uuser*';
-GRANT ALL PRIVILEGES ON DATABASE db_users TO uuser;
-
-CREATE USER uhotel WITH PASSWORD 'admin-password-uhotel*';
-GRANT ALL PRIVILEGES ON DATABASE db_hotel TO uhotel;
-
-CREATE USER upayment WITH PASSWORD 'admin-password-upayment*';
-GRANT ALL PRIVILEGES ON DATABASE db_payments TO upayment;
